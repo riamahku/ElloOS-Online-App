7054d0029836a7fa26bd5fd69c9af0db20c69338
Dian <52366863+riamahku@users.noreply.github.com>
2022-12-12 13:06:42 +0700

    Add files via upload

---
+++ bootloader.py
@@ -0,0 +1,31 @@
+import time,sys,os,rich
+from rich.prompt import Prompt, Confirm
+
+BOOTLOADER = True
+BOOTFALSE = False
+BOOTTRUE = True
+BOOTSECTION = hex(1)
+
+if BOOTLOADER == BOOTTRUE:
+    print("Booting on SC1")
+
+if BOOTLOADER == BOOTFALSE:
+    pass
+
+def quit_sys():
+    sys.exit(0)
+
+def load():
+    os.system("main.py")
+
+COPYRIGHT = "Copyright (c) SimpleBootloader V.1"
+string = COPYRIGHT.center(20)
+print(string)
+
+loader = Prompt.ask("Enter 1 to 3", choices=["ElloOS", "Safemode","Administrator"],default="ElloOS")
+if loader in("ElloOS"):
+    os.system("main.py")
+elif loader == "Safemode":
+    os.system("safemode.py")
+elif loader == "Administrator":
+    os.system("eldeploy.py")
